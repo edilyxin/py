8935dafa11cb7e71fabbf27f6283e6e6b06c9000
SheenX <edilyxin@sina.com>
2023-03-21 21:13:08 +0800

    first commit

---
+++ get_ip_utils.py
@@ -0,0 +1,123 @@
+import requests
+import time
+from bs4 import BeautifulSoup
+import re
+import json
+import DNS
+
+
+def getIpFromIpaddress(site):
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebkit/737.36(KHTML, like Gecke) Chrome/52.0.2743.82 Safari/537.36',
+               'Host': 'ipaddress.com'}
+    url = "https://ipaddress.com/website/" + site
+    trueip = None
+    try:
+        res = requests.get(url, headers=headers, timeout=30)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        ip = re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b", res.text)
+        result = soup.find_all('div', class_="comma-separated")
+        for c in result:
+            if len(ip) != 0:
+                trueip = ip[0]
+    except Exception as e:
+        print("查询" + site + " 时出现错误: " + str(e))
+    return trueip
+
+
+def getIpFromIp138(site):
+    # 从 site.ip138.com 查询ip
+    # headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebkit/737.36(KHTML, like Gecke) Chrome/52.0.2743.82 Safari/537.36',
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.41',
+        'Accept': '*/*',
+        'Cache-Control': 'no-cache',
+        'Host': 'site.ip138.com',
+        'Connection': 'keep-alive',
+        'Referer': 'https://site.ip138.com/%s/' % (site)
+    }
+    url = "https://site.ip138.com/domain/read.do?domain=" + site.lower() + "&time=" + str(time.time())
+    trueip = []
+    try:
+        res = requests.get(url, headers=headers, timeout=5, data={})
+        if (res.status_code == 200):
+            soup = json.loads(res.text)
+            ips = soup['data']  # type: ignore
+            for ip in ips:
+                trueip.append(ip['ip'])
+            return trueip
+    except Exception as e:
+        print("查询" + site + " 时出现错误: " + str(e))
+    return trueip
+
+
+def getIpFromChinaz(site):
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebkit/737.36(KHTML, like Gecke) Chrome/52.0.2743.82 Safari/537.36',
+               'Host': 'ip.tool.chinaz.com'}
+    url = "http://ip.tool.chinaz.com/" + site
+    trueip = None
+    try:
+        res = requests.get(url, headers=headers, timeout=5)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        result = soup.find_all('span', class_="Whwtdhalf w15-0")
+        for c in result:
+            ip = re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b", c.text)
+            if len(ip) != 0:
+                trueip = ip[0]
+    except Exception as e:
+        print("查询" + site + " 时出现错误: " + str(e))
+    return trueip
+
+
+def getIpFromWhatismyipaddress(site):
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebkit/737.36(KHTML, like Gecke) Chrome/52.0.2743.82 Safari/537.36',
+               'Host': 'ip.tool.chinaz.com'}
+    url = "https://whatismyipaddress.com//hostname-ip"
+    data = {
+        "DOMAINNAME": site,
+        "Lookup IP Address": "Lookup IP Address"
+    }
+    trueip = None
+    try:
+        res = requests.post(url, headers=headers, data=data, timeout=30)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        result = soup.find_all('span', class_="Whwtdhalf w15-0")
+        for c in result:
+            ip = re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b", c.text)
+            if len(ip) != 0:
+                trueip = ip[0]
+    except Exception as e:
+        print("查询" + site + " 时出现错误: " + str(e))
+    return trueip
+
+
+def getIpFromipapi(site):
+    '''
+    return trueip: None or ip
+    '''
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebkit/737.36(KHTML, like Gecke) Chrome/52.0.2743.82 Safari/537.36',
+               'Host': 'ip-api.com'}
+    url = "http://ip-api.com/json/%s?lang=zh-CN" % (site)
+    trueip = None
+    try:
+        res = requests.get(url, headers=headers, timeout=5)
+        res = json.loads(res.text)
+        if (res["status"] == "success"):
+            trueip = res["query"]
+    except Exception as e:
+        print("查询" + site + " 时出现错误: " + str(e))
+    return trueip
+
+
+def getTrueIp(site):
+    url = site
+    trueip = None
+    try:
+        res = DNS.dnslookup(url, qtype='A')  # convenience routine to return just answer data for any query type
+        if len(res) != 0:
+            trueip = res
+    except Exception as e:
+        print("query ip error is " + str(e))
+    return trueip
+
+
+print(getIpFromIp138('github.global.ssl.fastly.net'))
---
+++ main.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+import os, platform
+import shutil
+from datetime import datetime, timezone, timedelta
+import get_ip_utils
+
+# 需要获取ip的网址
+sites = ["github.global.ssl.fastly.net", "assets-cdn.github.com", "github.com"]
+
+addr2ip = {}
+hostLocation = r"./hosts"
+systemHostLocation = None
+
+
+def changeHostFile():
+    match platform.system():
+        case "Windows":
+            systemHostLocation = 'C:\\Windows\\System32\\drivers\\etc\\hosts'
+            os.remove(systemHostLocation + '.bak')
+            os.renames(systemHostLocation, systemHostLocation + '.bak')
+            shutil.copy(hostLocation, 'C:\\Windows\\System32\\drivers\\etc\\')
+            os.system("ipconfig /flushdns")
+        case "Linux":
+            systemHostLocation = '/etc/hosts'
+        case "Java":
+            systemHostLocation = '/root/hosts'
+        case _:
+            raise RuntimeError("Unknown operating system")
+
+
+def dropDuplication(line):
+    flag = False
+    if "#*******" in line:
+        return True
+    for site in sites:
+        if site in line:
+            flag = flag or True
+        else:
+            flag = flag or False
+    return flag
+
+
+# 更新host, 并刷新本地DNS
+def updateHost():
+    # today = datetime.date.today()
+    # rs = DNS.dnslookup('github.global.ssl.fastly.net', qtype='A')
+    # print(rs)
+
+    update_time = (
+        datetime.utcnow()
+        .astimezone(timezone(timedelta(hours=8)))
+        .replace(microsecond=0)
+        .isoformat()
+    )
+    for site in sites:
+        # trueIp is list
+        trueIp = get_ip_utils.getIpFromIp138(site)
+        if len(trueIp) > 0:
+            addr2ip[site] = trueIp
+            print(site + "\t" + ','.join(trueIp))
+        # if (platform.system() == 'Windows'):
+        with open(hostLocation, "r", encoding='utf8') as f1:
+            f1_lines = f1.readlines()
+            with open("tempHost", "w", encoding='utf8') as f2:
+                for line in f1_lines:  # 为了防止 host 越写用越长，需要删除之前更新的含有github相关内容
+                    if dropDuplication(line) is False:
+                        f2.write(line)
+                f2.write("#**********github " + update_time + " update **********\n")
+                for key in addr2ip:
+                    if len(addr2ip[key]) >= 1:  # 解决一个site有多个ip的情况
+                        for lineStr in addr2ip[key]:
+                            f2.write(lineStr + "\t" + key + "\n")
+                f2.write("#******* github hosts update end **********\n")
+    os.remove(hostLocation)
+    os.rename("temphost", hostLocation)
+    print('file update finish')
+    changeHostFile()
+
+
+if __name__ == "__main__":
+    updateHost()
+    print('update success')
+
+# Todo 根据不同的系统自动跟新host文件，需要搞明白什么是nds污染
