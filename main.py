02c72b3a2b4557bdd7b3646c99153975b510cd47
Sheen X <edilyxin@sina.com>
2023-03-22 17:21:59 +0800

    修改 import 写法

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-import os, platform
+import os
+import platform
 import shutil
 from datetime import datetime, timezone, timedelta
 import get_ip_utils
