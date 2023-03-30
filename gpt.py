b0acfaee2a9724470e41af9de5670f98c4b16cff
SheenX <edilyxin@sina.com>
2023-03-30 23:18:15 +0800

    add try exception

--- gpt.py
+++ gpt.py
@@ -1,4 +1,5 @@
 import requests
+# import openai
 import win32com.client as wincl  # pip install pywin32
 import json
 
@@ -8,7 +9,7 @@ speak = wincl.Dispatch("SAPI.SpVoice")
 url = "https://api.openai.com/v1/chat/completions"
 
 payload = '''{
-  "model":"gpt-3.5-turbo-0301","messages": [{"role": "user", "content": "程序员最需要修炼的"}]
+  "model":"gpt-3.5-turbo-0301","messages": [{"role": "user", "content": "vscode 中如何设置python的格式化插件"}]
 }'''
 
 payload = payload.encode("utf-8")
@@ -17,7 +18,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15',
     'Content-Type': 'application/json',
     'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
-    'Authorization': 'Bearer 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',
+    'Authorization': 'Bearer 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',
 }
 
 response = requests.request("POST", url, headers=headers, data=payload)
@@ -25,7 +26,10 @@ response = requests.request("POST", url, headers=headers, data=payload)
 print(response.text)
 
 gpt_response = json.loads(response.text)
-speak.Speak(gpt_response['choices'][0]['message']['content'].replace('\n\n', ''))
+try:
+    speak.Speak(gpt_response['choices'][0]['message']['content'].strip())
+except Exception as e:
+    print(str(e))
+# speak.Speak(gpt_response['choices'][0]['message']['content'].replace('\n\n', ''))
 # openai.organization = "org-9FucxtwNZHeS2hHTwnkj5brx"
-# openai.api_key = "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"
 # print(openai.Chat.)
