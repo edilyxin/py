6dd35074972913e1f9e172b873b096c95cb68222
Sheen X <edilyxin@sina.com>
2023-03-31 10:12:02 +0800

    my add

--- gpt.py
+++ gpt.py
@@ -1,18 +1,22 @@
 import requests
 
-url = "https://api.openai.com/v1/chat/completions"
+# url = "https://api.openai.com/v1/chat/completions"
+url = "https://api.openai.com/v1/images/generations"
 
+# "model":"gpt-3.5-turbo-0301","messages": [{"role": "user", "content": "神斗士星矢是谁创作的"}]
 payload = '''{
-  "model":"gpt-3.5-turbo-0301","messages": [{"role": "user", "content": "神斗士星矢是谁创作的"}]
+  "prompt": "以天气,龙黑洞,白虎为题材生成一张中国风的图片",
+  "n": 2,
+  "size": "1024x1024"
 }'''
 
 payload = payload.encode("utf-8")
 headers = {
-   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-   'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15',
-   'Content-Type': 'application/json',
-   'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
-   'Authorization': 'Bearer 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'
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15',
+    'Content-Type': 'application/json',
+    'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
+    'Authorization': 'Bearer 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',
 }
 
 response = requests.request("POST", url, headers=headers, data=payload)
